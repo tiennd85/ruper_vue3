36e1433aa7deb9fe95d126c74898bf6648af5571
Nguyen Duy Tien <tiennd19385@gmail.com>
2026-05-10 23:19:41 +0700

    a

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -47,10 +47,44 @@
 <script setup>
 import { onMounted } from 'vue'
 
+// Data Slides
+const { data: slidesData } = await useAsyncData('home-slides', () => 
+  queryContent('slides', 'slides_home1').findOne()
+)
+const slides = computed(() => slidesData.value?.body || [])
+
+// Data Banners1
+const { data: banners1Data } = await useAsyncData('home-banners-1', () => 
+  queryContent('banners', 'banners_home1_1').findOne()
+)
+const banners1 = computed(() => banners1Data.value?.body || [])
+
+// Data Banners2
+const { data: banners2Data } = await useAsyncData('home-banners-2', () => 
+  queryContent('banners', 'banners_home1_2').findOne()
+)
+const banners2 = computed(() => banners2Data.value?.body || [])
+
+// Data Videos
+const { data: videosData } = await useAsyncData('home-videos', () => 
+  queryContent('videos', 'videos_home1').findOne()
+)
+const videos = computed(() => videosData.value?.body || [])
+
+// Data Features
+const { data: featuresData } = await useAsyncData('home-features', () => 
+  queryContent('features', 'features').findOne()
+)
+const features = computed(() => featuresData.value?.body || [])
+
+useHead({
+  bodyAttrs: {
+    class: 'home'
+  }
+})
+
 onMounted(() => {
-  // Class of body tag
-  window.$('body').removeClass()
-  window.$('body').addClass('home')
+ 
 })
 
 /*
